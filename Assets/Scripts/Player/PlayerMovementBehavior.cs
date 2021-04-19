7a1f501145d0d0f2ad4a037b88bfecf6d0e33958
EthanJones19 <69812858+EthanJones19@users.noreply.github.com>
2021-04-19 16:58:54 -0500

    Initial commit

---
+++ Assets/Scripts/Player/PlayerMovementBehavior.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementBehavior : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    
+    public float JumpForce = 5.0f;
+
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        _rigidbody.AddForce(new Vector3(0, JumpForce, 0), ForceMode.Impulse);
+    }
+
+
+    public void OnCollisionStay(Collision collision)
+    {
+
+    }
+
+
+}
+
+
+
+
+
