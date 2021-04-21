1c7aba9d4b98ff5e9cede3699ac84641bf85e289
EthanJones19 <69812858+EthanJones19@users.noreply.github.com>
2021-04-20 22:50:34 -0500

    Added materials and added a wall

---
+++ Assets/Scripts/KillBlock/KillPlayerBehavior.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayerBehavior : MonoBehaviour
+{
+  
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject)
+            Destroy();
+
+    }
+
+    public void Destroy()
+    {
+        Destroy();
+    }
+}
---
+++ Assets/Scripts/Level/LevelMovementBehavior.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelMovementBehavior : MonoBehaviour
+{
+    public float HorizontalSpeed = 3;
+
+
+    void Update()
+    {
+        transform.position -= new Vector3(HorizontalSpeed, 0, 0) * Time.deltaTime;
+    }
+
+}
